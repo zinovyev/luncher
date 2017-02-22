6e934c304724cabe00cc09fea030449a6d26daf0
Ivan Zinovyev <vanyazin@gmail.com>
2017-02-23 01:00:51 +0300

    Main page greeting

--- spec/features/visit_main_page_spec.rb
+++ spec/features/visit_main_page_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'visiting the main page' do
   it 'greets the user' do
     visit '/'
-    expect(page).to have_current_path(main_page_path)
+    expect(page).to have_current_path(root_path)
     expect(page).to have_content 'Welcome'
   end
 end
