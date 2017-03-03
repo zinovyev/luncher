80afaafb5af81ca4723ba32c89171d43930a9fee
Ivan Zinovyev <vanyazin@gmail.com>
2017-03-03 09:43:41 +0300

    Orders to Prices association

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :user
-  has_one :price
+  # has_one :price
+  belongs_to :first_course, class_name: 'Price'
+  belongs_to :main_course, class_name: 'Price'
+  belongs_to :drink, class_name: 'Price'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170302220202) do
+ActiveRecord::Schema.define(version: 20170303064153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,11 +23,11 @@ ActiveRecord::Schema.define(version: 20170302220202) do
 
   create_table "orders", force: :cascade do |t|
     t.integer  "user_id"
-    t.integer  "first_course"
-    t.integer  "main_course"
-    t.integer  "drink"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.integer  "first_course_id"
+    t.integer  "main_course_id"
+    t.integer  "drink_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
   create_table "prices", force: :cascade do |t|
