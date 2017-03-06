ae1eb68c6d1cacf487180e8a8f70836c77ff40cb
Ivan Zinovyev <vanyazin@gmail.com>
2017-03-06 16:03:07 +0300

    Storage fog

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ CarrierWave.configure do |config|
       provider:              'AWS',                        # required
       aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],     # required
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], # required
-      aws_region:            ENV['AWS_REGION'],
+      region:                ENV['AWS_REGION'],
     }
     config.fog_directory  = 'luncher'                      # required
   else
