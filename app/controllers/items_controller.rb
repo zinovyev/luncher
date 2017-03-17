c4144ccc46a7b0c0df61b74937252d8c554271b7
Ivan Zinovyev <vanyazin@gmail.com>
2017-03-17 07:58:41 +0300

    Price destroy fix

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,25 +7,7 @@ class ItemsController < ApplicationController
     @items = Item.paginate(page: params[:page], per_page: 10)
   end
 
-  def new
-    @item = Item.new
-  end
-
-  def create
-    @item = Item.new(item_params)
-    if @item.save
-      flash[:success] = 'Item successfully created!'
-      redirect_to items_path
-    else
-      render :new
-    end
-  end
-
   private
-
-  def item_params
-    params.require(:item).permit(:title, :photo, :course, :image)
-  end
 
   def convert_course
     params[:item][:course] = params[:item][:course].to_i
